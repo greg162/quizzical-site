85cb8de75f4f3d21e30643ce5e681735d2c21f1a
Greg Thomas <greg@interactivetools.com>
2020-10-08 19:25:09 -0700

    Fixed typos in quiz component.

--- resources/js/quiz.js
+++ resources/js/quiz.js
@@ -16,9 +16,7 @@ import draggable from 'vuedraggable';
 // const files = require.context('./', true, /\.vue$/i)
 // files.keys().map(key => Vue.component(key.split('/').pop().split('.')[0], files(key).default))
 
-Vue.component('question-component', require('./components/QuestionTemplate.vue').default);
-Vue.component('question-component', require('./components/QuestionTemplate.vue').default);
-
+Vue.component('question-component', require('./components/questionTemplate.vue').default);
 
 /**
  * Next, we will create a fresh Vue application instance and attach it to
